f08af554ffd02a4b0c419aba0dee3dd9c5fecf6c
cherrry9 <cherrry9@disroot.org>
2020-05-11 12:17:22 +0200

    add file with layouts

--- .config/dwm/dwm-6.2/config.def.h
+++ .config/dwm/dwm-6.2/config.def.h
@@ -34,12 +34,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
+#include "layouts.c"
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-
-#include "fibonacci.c"
-#include "movestack.c"
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
