910a2e8b09743881612c81c808009c28ca98cb32
cherrry9 <cherrry9@disroot.org>
2020-06-21 19:52:07 +0200

    add emacs config in org mode

---
+++ .emacs.d/elfeed-feeds.el
@@ -0,0 +1,7 @@
+(setq elfeed-feeds
+			'(("https://www.archlinux.org/feeds/news/" linux)
+			("https://gynvael.coldwind.pl/rss_pl.php" blog)
+			("http://lukesmith.xyz/rss.xml" blog)
+			("https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA" blog)
+			("https://samedamci.me/blog/rss.xml" blog)
+			("https://notrelated.libsyn.com/rss" podcast)))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,56 +1,16 @@
-;; ui settings
-(scroll-bar-mode 0)
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-
-;; disable cursor blinking
-(blink-cursor-mode nil)
-
-;; line numbers
-(global-display-line-numbers-mode t)
-(setq display-line-numbers-type (quote relative))
-
-;; identation
-(setq tab-width 2
-      indent-tabs-mode nil)
-
-;; splash screen
-(setq inhibit-splash-screen t
-      initial-scratch-message nil
-      initial-major-mode 'org-mode)
-
-;; disable backups
-(setq make-backup-files nil)
-
-;; short yes / no to y / n
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; interactively do things 
-(ido-mode t)
-(setq ido-enable-flex-matching t
-      ido-use-virtual-buffers t)
-
-;; auto close bracket insertion
-(electric-pair-mode 1)
-;; match parenthesses
-(show-paren-mode t)
-;; make electric-pair-mode work on more brackets
-(setq electric-pair-pairs
-      '(
-        (?\" . ?\")
-        (?\{ . ?\})))
-
-;; packages
-(load "package")
-(package-initialize)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
+(org-babel-load-file "~/.emacs.d/README.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(custom-enabled-themes (quote (atom-one-dark)))
+ '(custom-safe-themes
+   (quote
+    ("d1af5ef9b24d25f50f00d455bd51c1d586ede1949c5d2863bef763c60ddf703a" default)))
+ '(package-selected-packages
+   (quote
+    (smex htmlize smartparens flycheck atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
