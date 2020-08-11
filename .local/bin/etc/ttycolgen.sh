1da9beb17ee3bf19462d363d605813441a2c2869
cherrry9 <cherrry9@disroot.org>
2020-08-11 09:51:23 +0200

    change tabs to spaces

--- .local/bin/etc/ttycolgen.sh
+++ .local/bin/etc/ttycolgen.sh
@@ -8,26 +8,26 @@ bg=$(xrdb -query | grep "\*.background" | awk -F "#" '{ print $2 }')
 
 echo "Writing to '$HOME/bin/ttycol.sh'"
 (
-	echo '#!/usr/bin/env bash'
-	echo 'export TERM=linux'
-	echo 'for tty in /dev/tty[0-9]; do'
+  echo '#!/usr/bin/env bash'
+  echo 'export TERM=linux'
+  echo 'for tty in /dev/tty[0-9]; do'
 
-	i=0
-	for c in $colours; do
-		case $i in
-		0) c=$bg; n=$i;;
-		10) n=A ;;
-		11) n=B ;;
-		12) n=C ;;
-		13) n=D ;;
-		14) n=E ;;
-		15) n=F ;;
-		*) n=$i ;;
-		esac
-		echo "echo -en \"\e]P$n$c\" > \"\$tty\""
-		i=$((i + 1))
-		[ "$i" -gt 15 ] && { echo "done";  exit; }
-	done
+  i=0
+  for c in $colours; do
+    case $i in
+    0) c=$bg; n=$i;;
+    10) n=A ;;
+    11) n=B ;;
+    12) n=C ;;
+    13) n=D ;;
+    14) n=E ;;
+    15) n=F ;;
+    *) n=$i ;;
+    esac
+    echo "echo -en \"\e]P$n$c\" > \"\$tty\""
+    i=$((i + 1))
+    [ "$i" -gt 15 ] && { echo "done";  exit; }
+  done
 
 ) >"$HOME/.cache/ttycol.sh"
 
